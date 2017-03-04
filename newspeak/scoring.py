89688e1f1528995c8f8b2454460c70c6fbd1029a
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-03-04 14:34:33 +0000

    fix spaces

--- newspeak/scoring.py
+++ newspeak/scoring.py
@@ -7,24 +7,25 @@ class Newspeak:
     @classmethod
 
     def clean_text(cls, text):
-    	""" Cleans text """
-    	tokenizer = RegexpTokenizer(r'(\w|\')+')
-		tokens = tokenizer.tokenize(text)
-		return tokens
+        """ Cleans text """
+        tokenizer = RegexpTokenizer(r'(\w|\')+')
+        tokens = tokenizer.tokenize(text)
+        return tokens
 
     def scoring(cls, newspeak_words, words_list):
-    	""" 
-    	Scores a list of words based on a newspeak words object 
-    	newspeak object:
+        """ 
+        Scores a list of words based on a newspeak words object 
+        newspeak object:
         TEST_CONFIG = {
             'words': ['find', 'me'],
             'suffix': ['ed'],
             'prefix': ['un', 'ante']
         }
-    	"""
+        """
         for word in words_list:
-            if has_word(word)
-
+            if has_word(word):
+                pass
+            
 
     def has_word(cls, config, word):
         """ Checks that article word is in newspeak config """
@@ -48,5 +49,4 @@ class Newspeak:
 
 
 if __name__== "__main__":
-
-    pe = Newspeak(record = rec)
+    pe = Newspeak(record=rec)
