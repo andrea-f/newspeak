5ff1cf9def6e6094b9e3ce089405b5b4e6947b0a
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-03-04 16:38:26 +0000

    add test

--- tests/test_reg_utils.py
+++ tests/test_reg_utils.py
@@ -10,3 +10,13 @@ from newspeak import regexp_utils
 ])
 def test_gen_regexp(typ, words, expr):
     assert regexp_utils.gen_regexp(typ, words).pattern == expr
+
+
+@pytest.mark.parametrize(('word', 'matches'), [
+    ('single',  True),
+    ('with space', True),
+    ('not found', False),
+])
+def test_full_match(word, matches):
+    members = ['with space', 'single']
+    assert bool(regexp_utils.gen_regexp('words', members).match(word)) == matches
